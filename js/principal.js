a1e4b5ba271e19d9122f87ab9069c1f683650da5
Jean Everton <jeanever39@gmail.com>
2021-07-08 14:56:16 -0300

    trabalhando com Atom

---
+++ js/principal.js
@@ -0,0 +1,18 @@
+var titulo = document.querySelector(".titulo");
+
+titulo.textContent = "Jean academy";
+// Variavel paciente e suas decendentes
+var paciente =  document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+var tdGordura = paciente.querySelector(".info-gordura");
+
+var altura = tdAltura.textContent;
+var peso = tdPeso.textContent;
+var gordura = tdGordura.textContent;
+
+console.log(paciente);
+console.log(tdPeso);
+console.log(peso);
+console.log(altura);
+console.log(gordura);
